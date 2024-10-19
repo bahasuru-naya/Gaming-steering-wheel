c94fd7d91b79e4b5aa491357492b38d71066af4f
Bahasuru Nayanakantha <p.h.d.bahasuru@gmail.com>
2024-10-19 23:53:16 +0530

    Add files via upload

---
+++ UnoJoyArduinoSample.ino
@@ -0,0 +1,130 @@
+#define ENC_A 2
+#define ENC_B 3
+#include "UnoJoy.h"
+
+
+unsigned long _lastIncReadTime = micros(); 
+unsigned long _lastDecReadTime = micros(); 
+int _pauseLength = 25000;
+int _fastIncrement = 10;
+
+volatile int counter = 128;  // Set the initial value to the midpoint (128)
+const int minCounter = 0;    // Minimum counter value
+const int maxCounter = 255;  // Maximum counter value
+
+
+void setup(){
+  pinMode(ENC_A, INPUT_PULLUP);
+  pinMode(ENC_B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENC_A), read_encoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC_B), read_encoder, CHANGE);
+  setupPins();
+  setupUnoJoy();
+}
+
+void loop(){
+  static int lastCounter = 0;
+
+  // If count has changed print the new value to serial
+  if(counter != lastCounter){    
+    lastCounter = counter;
+  }
+  // Always be getting fresh data
+  dataForController_t controllerData = getControllerData();
+  setControllerData(controllerData);
+}
+
+void setupPins(void){
+  // Set all the digital pins as inputs
+  // with the pull-up enabled, except for the 
+  // two serial line pins
+  for (int i = 2; i <= 12; i++){
+    pinMode(i, INPUT);
+    digitalWrite(i, HIGH);
+  }
+  pinMode(A4, INPUT);
+  digitalWrite(A4, HIGH);
+  pinMode(A5, INPUT);
+  digitalWrite(A5, HIGH);
+}
+
+dataForController_t getControllerData(void){
+  
+  // Set up a place for our controller data
+  //  Use the getBlankDataForController() function, since
+  //  just declaring a fresh dataForController_t tends
+  //  to get you one filled with junk from other, random
+  //  values that were in those memory locations before
+  dataForController_t controllerData = getBlankDataForController();
+  // Since our buttons are all held high and
+  //  pulled low when pressed, we use the "!"
+  //  operator to invert the readings from the pins
+  
+  controllerData.triangleOn = !digitalRead(4);
+  controllerData.circleOn = !digitalRead(5);
+  controllerData.dpadUpOn = !digitalRead(6);
+  controllerData.dpadDownOn = !digitalRead(7);
+  controllerData.dpadLeftOn = !digitalRead(8);
+  controllerData.dpadRightOn = !digitalRead(9);
+  controllerData.l1On = !digitalRead(10);
+  controllerData.r1On = !digitalRead(11);
+  controllerData.selectOn = !digitalRead(12);
+  controllerData.startOn = !digitalRead(A4);
+  controllerData.homeOn = !digitalRead(A5);
+  
+  // Set the analog sticks
+  //  Since analogRead(pin) returns a 10 bit value,
+  //  we need to perform a bit shift operation to
+  //  lose the 2 least significant bits and get an
+  //  8 bit number that we can use  
+  controllerData.leftStickX = counter;
+  
+  
+  // And return the data!
+  return controllerData;
+}
+
+
+void read_encoder() {
+  // Encoder interrupt routine for both pins. Updates counter
+  // if they are valid and have rotated a full indent
+ 
+  static uint8_t old_AB = 3;  // Lookup table index
+  static int8_t encval = 0;   // Encoder value  
+  static const int8_t enc_states[]  = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0}; // Lookup table
+
+  old_AB <<=2;  // Remember previous state
+
+  if (digitalRead(ENC_A)) old_AB |= 0x02; // Add current state of pin A
+  if (digitalRead(ENC_B)) old_AB |= 0x01; // Add current state of pin B
+  
+  encval += enc_states[( old_AB & 0x0f )];
+
+  // Update counter if encoder has rotated a full indent, that is at least 4 steps
+  if( encval > 3 ) {        // Four steps forward
+    int changevalue = 2;
+    if((micros() - _lastIncReadTime) < _pauseLength) {
+      changevalue = _fastIncrement * changevalue; 
+    }
+    _lastIncReadTime = micros();
+    counter += changevalue;  // Update counter
+    encval = 0;
+  }
+  else if( encval < -3 ) {   // Four steps backward
+    int changevalue = -2;
+    if((micros() - _lastDecReadTime) < _pauseLength) {
+      changevalue = _fastIncrement * changevalue; 
+    }
+    _lastDecReadTime = micros();
+    counter += changevalue;  // Update counter
+    encval = 0;
+  }
+
+  // Ensure counter stays within min and max limits
+  if(counter > maxCounter) {
+    counter = maxCounter;
+  } 
+  else if(counter < minCounter) {
+    counter = minCounter;
+  }
+}
